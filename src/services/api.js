6dbe63adfb58892cbd15e39deaadc7dedb3aba6a
feroz <ferozahmad5675@gmail.com>
2023-05-21 20:28:56 +0500

    api change

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 export const uploadFile = async (date) => {
   try {
-    let res = await axios.post("http://localhost:8000/upload", date);
+    let res = await axios.post(
+      "https://file-share-back.onrender.com/upload",
+      date
+    );
     return res.data;
   } catch (error) {
     console.error("error while Creating Api", error.message);
